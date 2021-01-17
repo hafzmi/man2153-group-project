c71f302fa54920208c6f155a8e45ab7226080f97
Phong Nguyen <phongnn@gmail.com>
2021-01-17 08:42:52 +0800

    load and save ANN

--- lib/wsf_files.py
+++ lib/wsf_files.py
@@ -1,5 +1,6 @@
 import joblib
 import pandas as pd
+from keras.models import model_from_json
 
 def wsf_save_train_dataset(X, y):
   X.to_csv('X_train.csv', index=False)
@@ -30,3 +31,24 @@ def wsf_save_best_model(model, name):
 
 def wsf_load_best_model(name):
   return joblib.load(name + '-best.model')
+
+def wsf_save_best_ann(ann):
+  # save weights to .h5 file
+  ann.save_weights("ann-best.h5")
+
+  # save model to json file
+  with open("ann-best.json", "w") as json_file:
+      json_file.write(ann.to_json())
+
+def wsf_load_best_ann():
+  # load model from json file
+  json_file = open('ann-best.json', 'r')
+  loaded_model_json = json_file.read()
+  json_file.close()
+
+  loaded_model = model_from_json(loaded_model_json)
+
+  # load weights into new model
+  loaded_model.load_weights("ann-best.h5")
+
+  return loaded_model
